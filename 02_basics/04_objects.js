6134cfaf88a66b6322f4cb8acc6a8a01f6b79265
himanshukumar735 <himanshukr864@gmail.com>
2026-01-10 18:28:22 +0530

    comming object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -51,9 +51,37 @@ const users = [
 
 // console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+const course = {
+    coursename: "JS in Hindi",
+    price: 999,
+    courseInstructor: "Hitesh Sir"
+}
+
+// console.log(course.courseInstructor);
+// console.log(course.coursename);
+console.log(course.price);
+
+const { courseInstructor } = course
+// console.log(courseInstructor);  // lets say we found courseInstructor too long to write. So,
+
+const { courseInstructor : instructor} = course  // This is called DeStructuring
+console.log(instructor);
+
+// {
+//     "name":"Hitesh Sir",
+//     "coursename": "JS in Hindi",
+//     "price": "free"
+// }
+
+// Its not mandatory that api response will always come in the form of object. It can come in the array also and within that array, there will be n number of objects and vice versa
+[
+    {},
+    {},
+    {}
+]
